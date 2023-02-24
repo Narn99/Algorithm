ac3ee55debc316e8756320dec29e5705d58a8c49
Narn99 <123129555+Narn99@users.noreply.github.com>
2023-02-24 16:26:36 +0900

    [Bronze I] Title: 색종이, Time: 268 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -4,13 +4,19 @@
 N = int(input())
 arr = [[-1]*1001 for _ in range(1001)]  # 1001*1001 격자
 
+mn_i = 1001
+mn_j = 1001
 mx_i = 0
 mx_j = 0
 
 for paper in range(N) :
     a, b, c, d = map(int,input().split())
+    if a < mn_i :
+        mn_i = a        # 시간 줄이기용 최소 범위 구하기 조건
+    if b < mn_j :
+        mn_j = b
     if a+c > mx_i :
-        mx_i = a+c      # 시간 줄이기용 범위 구하기 조건
+        mx_i = a+c      # 시간 줄이기용 최대 범위 구하기 조건
     if b+d > mx_j :
         mx_j = b+d
     for i in range(a, a+c) :
@@ -20,8 +26,8 @@ for paper in range(N) :
 ans = []
 for paper in range(N) :
     cnt = 0
-    for i in range(mx_i) :
-        for j in range(mx_j) :
+    for i in range(mn_i, mx_i) :    # 시간 줄이기 위해 정해진 범위만 탐색
+        for j in range(mn_j, mx_j) :
             if arr[i][j] == paper :     # 다 붙인 뒤의 색종이 면적 구해서 기록
                 cnt += 1
     else :
