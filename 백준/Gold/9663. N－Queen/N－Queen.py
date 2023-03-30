c6cd900863446d3ea31b5a0a7c538b3bacda2abe
Narn99 <123129555+Narn99@users.noreply.github.com>
2023-03-30 11:55:14 +0900

    [Gold IV] Title: N-Queen, Time: 14108 ms, Memory: 150764 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -2,7 +2,6 @@
 # N행 N열에 N개의 퀸을 놓는다면, 각 행, 각 열에 하나의 퀸만 존재해야함
 # 방문한 행과 열을 기록해서 제외시키고, 나머지 중에서 대각선 탐색으로 다른게 없으면 두고 백트래킹?
 import sys
-from collections import deque
 
 def check(n, r, C, d1, d2) :
     global ans
@@ -14,20 +13,14 @@ def check(n, r, C, d1, d2) :
     for j in range(N) :     # 현재 행에서 사용 안 한 열에서, 대각선 체크하고 괜찮으면 둔다.
         if j not in C :
             if (r+j) not in d1 and (j-r) not in d2 :
-                C.append(j)
-                d1.append(r+j)
-                d2.append(j-r)
-                check(n+1, r+1, C, d1, d2)
-                C.pop()
-                d1.pop()
-                d2.pop()
+                check(n+1, r+1, C+[j], d1 + [r+j], d2 + [j-r])
 
 N = int(sys.stdin.readline())
+d1 = []  # 대각선과 열 기록할 리스트
+d2 = []
+C = []
 ans = 0
-C = deque()
-d1 = deque()
-d2 = deque()
 
-check(0, 0, C, d1, d2)
+check(0, 0, C , d1, d2)
 
 print(ans)
